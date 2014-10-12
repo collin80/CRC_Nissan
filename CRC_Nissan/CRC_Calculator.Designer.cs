588657952e288734a213b599ae272bc2759e5e90
Collin Kidder <collink@kkmfg.com>
2014-10-12 14:29:55 -0400

    Initial Commit

---
+++ CRC_Nissan/CRC_Calculator.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CRC_Nissan
+{
+    partial class CRC_Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPoly = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBytes = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Poly:";
+            // 
+            // txtPoly
+            // 
+            this.txtPoly.Location = new System.Drawing.Point(39, 37);
+            this.txtPoly.Name = "txtPoly";
+            this.txtPoly.Size = new System.Drawing.Size(100, 20);
+            this.txtPoly.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "7 Input Bytes (Separate With Spaces):";
+            // 
+            // txtBytes
+            // 
+            this.txtBytes.Location = new System.Drawing.Point(198, 70);
+            this.txtBytes.Name = "txtBytes";
+            this.txtBytes.Size = new System.Drawing.Size(120, 20);
+            this.txtBytes.TabIndex = 3;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(75, 96);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output CRC:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "All values in hexadecimal but don\'t use 0x";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(15, 135);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(302, 23);
+            this.btnCalc.TabIndex = 7;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CRC_Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 175);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBytes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPoly);
+            this.Controls.Add(this.label1);
+            this.Name = "CRC_Calculator";
+            this.Text = "CRC Nissan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPoly;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBytes;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ CRC_Nissan/CRC_Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CRC_Nissan
+{
+    public partial class CRC_Calculator : Form
+    {
+        public CRC_Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //txtPoly has the poly
+            //txtBytes has the 7 input bytes
+            //txtOutput is used for output CRC
+
+            uint poly = uint.Parse(txtPoly.Text, System.Globalization.NumberStyles.HexNumber);
+            byte CRCOutput = 0;
+            byte[] input_bytes = new byte[8];
+
+            char[] splitters = { ' ' };
+            String[] Tokens = txtBytes.Text.Split(splitters);
+            if (Tokens.Length < 7)
+            {
+                MessageBox.Show("You need to enter 7 bytes here separated by spaces");
+                return;
+            }
+
+            for (int i = 0; i < 7; i++) 
+                input_bytes[i] = byte.Parse(Tokens[i], System.Globalization.NumberStyles.HexNumber);
+
+            input_bytes[7] = 0;
+
+            //start with msb of each byte and process the same every time. 
+
+            byte thisbit = 0;
+            for (int b = 0; b < 8; b++)
+            {
+                for (int bit = 7; bit >= 0; bit--)
+                {
+                    thisbit = 0;
+                    if ((input_bytes[b] & (1 << bit)) > 0) thisbit = 1;
+                    if (CRCOutput > 127) 
+                    {
+                        CRCOutput = (byte)(((CRCOutput << 1) + thisbit) ^ poly);
+                    }
+                    else 
+                    {
+                        CRCOutput = (byte)((CRCOutput << 1) + thisbit);
+                    }                    
+                }
+            }
+            txtOutput.Text = CRCOutput.ToString("X2");
+        }
+    }
+}
